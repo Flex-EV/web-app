c716a072914a0dbc7c470eeb8681697fc6fe82d1
Pranay-Gupta <pranaygupta.dgp@gmail.com>
2024-10-20 23:11:08 +0530

    test lint staged

--- src/pages/earning-details/EarningDetails.tsx
+++ src/pages/earning-details/EarningDetails.tsx
@@ -1,5 +1,5 @@
 const EarningDetails = () => {
-  return <div className="text-neutral-100">EarningDetails</div>;
+  return <div className="text-neutral-100">Earning Details</div>;
 };
 
 export default EarningDetails;
