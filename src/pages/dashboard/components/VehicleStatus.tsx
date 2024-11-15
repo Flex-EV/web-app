3f9cd1ca055456391dd9bb0af11ebe41343a2e82
Pranay-Gupta <pranaygupta.dgp@gmail.com>
2024-11-15 23:06:16 +0530

    test

--- src/pages/dashboard/components/VehicleStatus.tsx
+++ src/pages/dashboard/components/VehicleStatus.tsx
@@ -8,7 +8,7 @@ const VehicleStatus = () => {
           key={index}
           className={`${status.color} p-6 rounded-lg text-white shadow-lg`}
         >
-          <h3 className="text-lg font-semibold mb-2">{status.label}</h3>
+          <h3 className="text-lg font-semibold mb-3">{status.label}</h3>
           <p className="text-4xl font-bold">{status.count}</p>
         </div>
       ))}
