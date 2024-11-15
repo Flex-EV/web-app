40aa95abb79dedab75fd205af6b08b77f970733d
Pranay-Gupta <pranaygupta.dgp@gmail.com>
2024-11-15 20:52:24 +0530

    test

--- src/pages/dashboard/components/Map.tsx
+++ src/pages/dashboard/components/Map.tsx
@@ -4,6 +4,7 @@ const Map = () => {
   return (
     <div style={{ height: '100%', width: '100%' }}>
       <MapContainer
+      
         center={[19.02680514347375, 73.03826439027594]}
         zoom={17}
         style={{ height: '100%', width: '100%' }}
