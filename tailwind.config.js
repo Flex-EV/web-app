2ac3d42602ff6bbcabffa8827fa81c9aa6ae7bc1
Harshvardhan Sharma <harshvardhans742@gmail.com>
2024-11-19 19:22:07 +0530

    Created a regex for phone number and required fields

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			poppins: ['Poppins', 'sans-serif'],
-  			spaceGrotesk: ['Space Grotesk', 'sans-serif']
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        spaceGrotesk: ['Space Grotesk', 'sans-serif'],
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
   },
-  
 };
