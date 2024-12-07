b6715e185617f170606425f98df8e99906bf7332
Pranay-Gupta <pranaygupta.dgp@gmail.com>
2024-12-07 21:01:23 +0530

    fix

--- src/pages/rider-management/service/RiderManagementService.ts
+++ src/pages/rider-management/service/RiderManagementService.ts
@@ -1,25 +1,25 @@
 import axios from 'axios';
 import {
+  GetRidersResponse,
   RiderFilterRequest,
-  Riders,
 } from '@/pages/rider-management/model/Riders.interface.ts';
 import { v4 as uuid } from 'uuid';
 import {
   AddRiderData,
   AddRiderDataResponse,
-} from '@/pages/rider-management/model/AddRider.interface.ts'; // Importing UUID library
+} from '@/pages/rider-management/model/AddRider.interface.ts';
+
+const RIDER_MANAGEMENT_BASE_URL =
+  'http://3.109.1.118:8080/agent-api/v1/rest/riders';
 
 class RiderManagementService {
-  private static RIDER_MANAGEMENT_BASE_URL =
-    'http://3.109.1.118:8080/agent-api/v1/rest/riders';
-
   public static async fetchRiders(
-    page: number = 0,
-    size: number = 20,
+    page = 0,
+    size = 20,
     filter: RiderFilterRequest = {}
-  ): Promise<Riders> {
+  ): Promise<GetRidersResponse> {
     const response = await axios.post(
-      `${this.RIDER_MANAGEMENT_BASE_URL}/all?page=${page}&size=${size}`,
+      `${RIDER_MANAGEMENT_BASE_URL}/all?page=${page}&size=${size}`,
       { data: filter },
       {
         headers: {
@@ -62,17 +62,13 @@ class RiderManagementService {
 
     const idempotencyKey = uuid();
 
-    const response = await axios.post(
-      this.RIDER_MANAGEMENT_BASE_URL,
-      formData,
-      {
-        headers: {
-          accept: 'application/vnd.flex-ev.rider+json;version=1',
-          'Idempotency-Key': idempotencyKey,
-          'Content-Type': 'multipart/form-data',
-        },
-      }
-    );
+    const response = await axios.post(RIDER_MANAGEMENT_BASE_URL, formData, {
+      headers: {
+        accept: 'application/vnd.flex-ev.rider+json;version=1',
+        'Idempotency-Key': idempotencyKey,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
     return response.data;
   };
 }
