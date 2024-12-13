8351d6040f8e2b7f4d325dd8737da98f8b02a8f5
Pranay-Gupta <pranaygupta.dgp@gmail.com>
2024-12-13 19:47:45 +0530

    fixed

---
+++ src/routes/util/routing.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import { AppRoutes } from '@/routes/enum/Routes.enum.ts';
+
+export const useCustomNavigate = () => {
+  const navigate = useNavigate();
+
+  return {
+    toDashboard: () => navigate(AppRoutes.DASHBOARD),
+    toRiderDetails: (riderId: string) => {
+      navigate(`${AppRoutes.RIDER_MANAGEMENT}/${riderId}`);
+    },
+  };
+};
