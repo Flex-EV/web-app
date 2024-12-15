fe6c14031fe83157fd545bd1f6fa586660ec435c
Pranay-Gupta <pranaygupta.dgp@gmail.com>
2024-12-15 16:59:01 +0530

    Update domain

--- src/modules/rider-management/service/RiderManagementService.ts
+++ src/modules/rider-management/service/RiderManagementService.ts
@@ -11,7 +11,7 @@ import {
 } from '@/modules/rider-management/model/AddRider.interface.ts';
 
 const RIDER_MANAGEMENT_BASE_URL =
-  'http://65.0.74.170:8080/agent-api/v1/rest/riders';
+  'https://dev-api.flex-ev.tech:8080/agent-api/v1/rest/riders';
 
 const RiderManagementService = {
   // Get all riders
